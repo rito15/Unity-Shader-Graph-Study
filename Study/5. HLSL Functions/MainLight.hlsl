fb4b3bf9efeaa091390a6026c724cb1536dcca0f
rito15 <ghdtkals15@naver.com>
2020-10-06 18:39:02 +0900

    [2020-10-06] Grayscale

--- Study/5. HLSL Functions/MainLight.hlsl
+++ Study/5. HLSL Functions/MainLight.hlsl
@@ -1,7 +1,13 @@
 void GetLightingInformation_float(out float3 Direction, out float3 Color,out float Attenuation)
 {
-    Light light = GetMainLight();
-    Direction = light.direction;
-    Attenuation = light.distanceAttenuation;
-    Color = light.color;
+    #ifdef SHADERGRAPH_PREVIEW
+        Direction = float3(-0.5,0.5,-0.5);
+        Color = float3(1,1,1);
+        Attenuation = 0.4;
+    #else
+        Light light = GetMainLight();
+        Direction = light.direction;
+        Attenuation = light.distanceAttenuation;
+        Color = light.color;
+    #endif
 }
